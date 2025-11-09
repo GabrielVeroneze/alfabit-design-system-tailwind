d2b117a7f71632ce5ad8d5283086709660e5366c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-09 13:43:36 -0300

    📚 docs: adicionar story inicial do componente Button no Storybook
    Cria o arquivo de story do componente Button no formato compatível com o Storybook e Next.js Vite. Define metadados como título, tags e componente principal, além de um exemplo inicial (Primary)
    que exibe o botão com o texto “Botão”. Esse story serve como base para a documentação e visualização interativa do componente no Storybook.

---
+++ components/Button/Button.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/nextjs-vite'
+import Button, { ButtonProps } from './Button'
+
+const meta = {
+    title: 'Button',
+    tags: ['autodocs'],
+    component: Button,
+    argTypes: {},
+} satisfies Meta<ButtonProps>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+        children: 'Botão',
+    },
+}
