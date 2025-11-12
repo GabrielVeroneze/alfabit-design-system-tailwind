1fc45827f65c93233d49435f57ec25729e7aaa3f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-12 16:46:16 -0300

    ✨ feat: aprimorar componente Input com label, estilos e suporte a estado desabilitado
    Refatora o componente Input para incluir suporte a rótulo (label), aplicar classes de estilo
    personalizadas e adicionar tratamento visual para o estado desabilitado. Introduz também o
    componente interno InputOrTextarea para lidar com a renderização condicional entre input e textarea.

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -4,13 +4,13 @@ type InputElementProps =
     | InputHTMLAttributes<HTMLInputElement>
     | TextareaHTMLAttributes<HTMLTextAreaElement>
 
-type InputProps = InputElementProps & {
+export type InputProps = InputElementProps & {
     label?: string
     multiline?: boolean
 }
 
-const Input = ({ multiline, ...props }: InputProps) => {
-    if (multiline) {
+const InputOrTextarea = (props: InputProps) => {
+    if (props.multiline) {
         return (
             <textarea
                 {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
@@ -19,6 +19,22 @@ const Input = ({ multiline, ...props }: InputProps) => {
     }
 
     return <input {...(props as InputHTMLAttributes<HTMLInputElement>)} />
+}
+
+const Input = ({ label, className, ...rest }: InputProps) => {
+    return (
+        <div className="relative">
+            {label && <label className="absolute">{label}</label>}
+            <InputOrTextarea
+                className={`
+                    bg-dark text-gray-secondary
+                    disabled:bg-disabled disabled:text-disabled disabled:border-2 disabled:border-disabled
+                    ${className}
+                `}
+                {...rest}
+            />
+        </div>
+    )
 }
 
 export default Input
