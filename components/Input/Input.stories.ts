cbd37ef9bcf85286b800c256396f676cb0685e9f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-12 17:41:11 -0300

    ✨ feat: adiciona variações de estado e layout para o componente Input no Storybook
    Expande as histórias do componente Input no Storybook, adicionando novas variações
    para diferentes combinações de propriedades. Inclui versões com e sem label, em modos multilinha e desabilitado. Agora o componente possui histórias para Primary, Multiline, Label e suas respectivas combinações com o estado Disabled.

--- components/Input/Input.stories.ts
+++ components/Input/Input.stories.ts
@@ -17,9 +17,56 @@ export const Primary: Story = {
     },
 }
 
-export const Disabled: Story = {
+export const PrimaryDisabled: Story = {
     args: {
         value: 'Input',
+        disabled: true,
+    },
+}
+
+export const Multiline: Story = {
+    args: {
+        value: 'Input',
+        multiline: true,
+    },
+}
+
+export const MultilineDisabled: Story = {
+    args: {
+        value: 'Input',
+        multiline: true,
+        disabled: true,
+    },
+}
+
+export const PrimaryLabel: Story = {
+    args: {
+        value: 'Input',
+        label: 'Label',
+    },
+}
+
+export const PrimaryLabelDisabled: Story = {
+    args: {
+        value: 'Input',
+        label: 'Label',
+        disabled: true,
+    },
+}
+
+export const MultilineLabel: Story = {
+    args: {
+        value: 'Input',
+        multiline: true,
+        label: 'Label',
+    },
+}
+
+export const MultilineLabelDisabled: Story = {
+    args: {
+        value: 'Input',
+        multiline: true,
+        label: 'Label',
         disabled: true,
     },
 }
