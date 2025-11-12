3f7a0a2b87da3db77583fed2988defd6ca7eac45
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-11-12 17:36:06 -0300

    ✨ feat: aprimora estilo e posicionamento do label no componente Input
    Ajusta o componente Input para melhorar a aparência e o espaçamento do label. Adiciona
    padding superior e à esquerda no texto do label, define um tamanho de fonte menor e melhora a legibilidade. Também inclui bordas arredondadas e padding interno no campo de input/textarea, além de aplicar espaçamento condicional quando o label estiver
    presente.

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -24,11 +24,14 @@ const InputOrTextarea = (props: InputProps) => {
 const Input = ({ label, className, ...rest }: InputProps) => {
     return (
         <div className="relative">
-            {label && <label className="absolute">{label}</label>}
+            {label && (
+                <label className="absolute pt-1 pl-3 text-xs">{label}</label>
+            )}
             <InputOrTextarea
                 className={`
-                    bg-dark text-gray-secondary
+                    bg-dark text-gray-secondary rounded-md p-3
                     disabled:bg-disabled disabled:text-disabled disabled:border-2 disabled:border-disabled
+                    ${label && 'pt-5'}
                     ${className}
                 `}
                 {...rest}
